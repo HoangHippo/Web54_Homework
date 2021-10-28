7257b274fb6625c2bbb032695afc62b7dd3423c6
HoangHippo <hoanghippo17@gmail.com>
2021-10-28 21:48:48 +0700

    Hoang update

---
+++ Homework_L1/Ex_4/index.js
@@ -0,0 +1,56 @@
+let button = document.getElementById("enter");
+let input = document.getElementById("userinput");
+let ul = document.querySelector("ul");
+
+function inputLength() {
+	return input.value.length;
+}
+
+function createListElement() {
+	let li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
+	input.value = "";
+
+
+	let btn = document.createElement("button");
+    btn.className = 'btn btn-danger'
+	btn.appendChild(document.createTextNode("Delete"));
+	li.appendChild(btn);
+}
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
+}
+
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
+function done(eventClick){
+	eventClick.classList.toggle("done");
+}
+
+function deleteItem(eventClick){
+	let deleteBtns = document.getElementsByClassName("delete");
+	let ul = document.querySelector("ul");
+	ul.removeChild(eventClick.parentNode);
+}
+
+function doneAndDeleteClick(){
+	let eventClick = event.target;
+	if(eventClick.tagName === "LI"){
+		done(eventClick);
+	}
+	else if(eventClick.tagName === "BUTTON"){
+		deleteItem(eventClick);
+	}
+}
+
+button.addEventListener("click", addListAfterClick);
+input.addEventListener("keypress", addListAfterKeypress);
+ul.addEventListener("click", doneAndDeleteClick);
